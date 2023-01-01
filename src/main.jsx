051cfa09645708327e0cb02794225ee2ee501f02
talentlessDeveloper <kareemope52@gmail.com>
2023-01-01 16:07:47 +0100

    installed react-query and modified loader color

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,17 @@ import ThemeProvider from "./context/ThemeContext";
 import GithubRepos from "./GithubRepos";
 
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <BrowserRouter>
-        <GithubRepos />
-      </BrowserRouter>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <BrowserRouter>
+          <GithubRepos />
+        </BrowserRouter>
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- src/utility/Loader.jsx
+++ src/utility/Loader.jsx
@@ -2,8 +2,8 @@ import { RotateLoader } from "react-spinners";
 
 const Loader = () => {
   return (
-    <div className='h-[80vh] w-[90vw] md:w-[50vw] flex items-center justify-center'>
-      <RotateLoader color='rgb(45, 164, 78)' />
+    <div className='h-[80vh] w-full md:w-1/2 flex items-center justify-center'>
+      <RotateLoader color='rgb(36, 41, 47)' />
     </div>
   );
 };
